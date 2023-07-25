114a4f34551e80691a0fdeed31f7d604abdefc2e
sirus-max <pradeepkumar96433@gmail.com>
2023-07-25 10:27:30 +0530

    kajsfd

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -3,18 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import Event_details from "./components/Pages/Events/Event_List/Event_list"
 import Event_detail from "./components/Pages/Events/Event_details/Event_detail"
 import LoginForm from './components/Pages/Login/Login';
-import SignUpForm from '/Users/pradeepkumar/Desktop/Techniche_23/client/src/components/Pages/SignUp/SignUp';
-import Event_list from './components/Pages/Events/Event_List/Event_list';
+import SignUpForm from './components/Pages/SignUp/SignUp'
+import EventList from './components/Pages/Events/Event_List/Event_list';
 import Card from './components/Pages/Events/Event_List/components_and_images/card';
 import Team from './components/Pages/Team/Team';
-
+import Home from './components/Pages/Home/Home';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home/>} /> */}
+      <Route path="/" element={<Home/>} />
 
-      <Route path="/events" element={<Event_list />} />
+      <Route path="/events" element={<EventList />} />
             {/* individual events */}
             {/* <Route path="/lecture_series" element={<Lecture_series />} /> */}
             {/* <Route path="/nexus" element={<Nexus />} /> */}
---
+++ src/components/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <div className='text-white mt-40'>e;afk;sd</div>
+    </div>
+  )
+}
+
+export default Home
