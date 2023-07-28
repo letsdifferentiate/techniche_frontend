1afd1e18a213a8a1eb1e551e56ceffe507e4b26c
sirus-max <pradeepkumar96433@gmail.com>
2023-07-28 16:36:13 +0530

    home page aditions

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -12,6 +12,7 @@ import globe from "./images/globe.mp4"
 import coordinates from "./images/Coordinates.png"
 import flower from "./images/flower.png"
 import "./Home.css"
+import Logo from './Logo'
 
 function Home() {
 
@@ -29,13 +30,25 @@ videoPlayer.oncanplay = () => {
 
   return (
     <div>
+    <Logo />
+
+
       <div className='text-white mt-40 w-[80vw] h-[50vh] mx-auto relative flex justify-between'>
-        <img src={sidebar} className='h-inherit'/>
-      
+       
         <div className=' w-[40%] relative my-auto'>
             <div>SINCE 1999</div>
-          <div className='h-[90vh] w-[90vh] -ml-20 '>
-            <img src={Techniche} />
+          <div className='h-[90vh] w-[90vh] -ml-20 techniche'>
+              <div className='text-9xl text-white flex'>
+                  <div className='mori_to_grid text-blue-800 T1'>T</div>
+                  <div className='grid_to_mori text-violet-800 E1'>E</div>
+                  <div className='mori_to_grid text-blue-500 C1'>C</div>
+                  <div className='grid_to_mori  H1'>H</div>
+                  <div className='mori_to_grid N1'>N</div>
+                  <div className='mori_to_grid I1'>I</div>
+                  <div className='grid_to_mori C2'>C</div>
+                  <div className='mori_to_grid H2'>H</div>
+                  <div className='mori_to_grid E2'>E</div>
+              </div>
             </div>
             {/* <div className='absolute right-0 flex justify-end flex-col'>
              
@@ -45,32 +58,30 @@ videoPlayer.oncanplay = () => {
             </div> */}
             
         </div>
-        <img src={Techniche_logo} className='h-[60%] my-auto'/>
-        <img src={sidebar} className='h-inherit'/>
+        
+    
       </div>
 
 
       <a href="https://www.google.com/maps/d/u/0/viewer?mid=10kOD2TXBPY1qsMQfdF8AXlBxS0Q&hl=en_US&ll=26.19210399999995%2C91.69464100000002&z=17" className='text-white'>
-      <div className='mt-9 w-[80vw] h-auto rounded-full border-1 border white mx-auto flex justify-between relative z-1'>
-            
-        <div className='m-2 ml-7 flex'>
-          
-            <div className='w-[10vh] h-[10vh]'>
-            <video id="videoPlayer" loop autoPlay muted>
-                  <source src={globe} type="video/mp4" />
-            </video>
-            </div>  
-            <div className='text-white my-auto ml-3'>THE 25TH EDITION <br/>26°11′14″N,  91°41′30″E</div>
-           
-        </div>
-        <div className='flex'>
-            <div className='text-white my-auto'>The Annual Techno-Management festival of Indian Institute of Technology (IIT), Guwahati. <br/> This year we bring to you the 25th Edition.</div>
-            
-            <img src={flower} className='w-[8vh] h-[8vh] mx-4 my-auto animate-spin'/></div>
-            
-      </div></a>
+          <div className='mt-9 w-[80vw] h-auto rounded-full border-1 border white mx-auto flex justify-between relative z-1'>
+            <div className='m-2 ml-7 flex'>
+                <div className='w-[10vh] h-[10vh]'>
+                <video id="videoPlayer" loop autoPlay muted>
+                      <source src={globe} type="video/mp4" />
+                </video>
+                </div>  
+                <div className='text-white my-auto ml-3'>THE 25TH EDITION <br/>26°11′14″N,  91°41′30″E</div>
+            </div>
+            <div className='flex'>
+                <div className='text-white my-auto'>The Annual Techno-Management festival of Indian Institute of Technology (IIT), Guwahati. <br/> This year we bring to you the 25th Edition.</div>
+                <img src={flower} className='w-[8vh] h-[8vh] mx-4 my-auto animate-spin'/></div>
+          </div>
+      </a>
+
+
       <div className='w-[80vw]  mx-auto my-10 h-[80vh]'>
-        <ReactPlayer url="https://www.youtube.com/watch?v=rMe52W-dRbQ" playing={true} width="100%" height="100%" />
+        <ReactPlayer url="https://www.youtub.com/watch?v=rMe52W-dRbQ" playing={true} width="100%" height="100%" />
         
       </div>
 
---
+++ src/components/Pages/Home/Logo.js
@@ -0,0 +1,37 @@
+import React, {useState,useEffect} from 'react'
+import Techniche_logo from "./images/Techniche_logo.svg"
+
+
+function Logo() {
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  // Update scroll position when the user scrolls
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    // Add event listener to track scroll position
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+
+
+  return (
+    <div className='fixed right-[30vh] h-[5vh]'>
+      <div className={scrollPosition ? '' : 'rotate'} style={{ transform: `translateX(${-1*scrollPosition * 0.3}px) translateY(${scrollPosition * 0.2}px) rotateX(${scrollPosition * 0.15}deg) rotateY(${scrollPosition * 0.3}deg)` }}>
+        <img src={Techniche_logo} className='h-[40vh]'/>
+      </div>
+        
+    </div>
+  )
+}
+
+export default Logo
