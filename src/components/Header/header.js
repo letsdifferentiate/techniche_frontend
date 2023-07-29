e0052b2fec54542b5b57eb61b8706dabba22cebc
sirus-max <pradeepkumar96433@gmail.com>
2023-07-29 11:10:14 +0530

    changing header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -5,8 +5,131 @@ import { Link } from 'react-router-dom'
 
 
 function Header() {
-  const [displayText, setDisplayText] = useState("Evesnts")
-  
+  const [eventsText, setEventsText] = useState('Events');
+  const [competitionText, setCompetitionText] = useState('Competitions');
+  const [workshopsText, setWorkshopsText] = useState('Workshops');
+  const [ambassadorText, setAmbassadorText] = useState('Campus Ambassdor');
+
+/////////////////////////////////////////////////////
+  useEffect(() => {
+    let timeoutId;
+    const changeEventText = () => {
+      setEventsText('Eve^<!');
+      timeoutId = setTimeout(() => {
+        setEventsText('Ev~1>>');
+        timeoutId = setTimeout(() => {
+          setEventsText('Eve!!>');
+          timeoutId = setTimeout(() => {
+            setEventsText('EVENTS');
+          },150)
+        }, 150);
+      }, 150);
+    };
+
+    const stopEventText = () => {
+      clearTimeout(timeoutId);
+      setEventsText('Events');
+    };
+//////////////////////////////////////////////////////////
+    const changeCompetitionText = () => {
+      setCompetitionText('Compet>>>!!');
+      timeoutId = setTimeout(() => {
+        setCompetitionText('Compet<<!!>');
+        timeoutId = setTimeout(() => {
+          setCompetitionText('Compet^^^><');
+          timeoutId = setTimeout(() => {
+            setCompetitionText('COMPETITIONS');
+          },150)
+        }, 150);
+      }, 150);
+    };
+
+    const stopCompetitionText = () => {
+      clearTimeout(timeoutId);
+      setCompetitionText('Competitions');
+    };
+//////////////////////////////////////////////
+    const changeWorkshopText = () => {
+      setWorkshopsText('Work<>^<>');
+      timeoutId = setTimeout(() => {
+        setWorkshopsText('Wor..|..');
+        timeoutId = setTimeout(() => {
+          setWorkshopsText('Wo><.?|');
+          timeoutId = setTimeout(() => {
+            setWorkshopsText('WORKSHOPS');
+          },150)
+        }, 150);
+      }, 150);
+    };
+
+    const stopWorkshopText = () => {
+      clearTimeout(timeoutId);
+      setWorkshopsText('Workshops');
+    };
+//////////////////////////////////////////////
+const changeAmbassdorText = () => {
+  setAmbassadorText('Campus ........');
+  timeoutId = setTimeout(() => {
+    setAmbassadorText('Campus >>>>>>>');
+    timeoutId = setTimeout(() => {
+      setAmbassadorText('Campus <<<<<<<');
+      timeoutId = setTimeout(() => {
+        setAmbassadorText('CAMPUS AMBASSDOR');
+      },150)
+    }, 150);
+  }, 150);
+};
+
+const stopAmbassdorText = () => {
+  clearTimeout(timeoutId);
+  setAmbassadorText('Campus Ambassdor');
+};
+//////////////////////////////////////////////
+
+    const eventsElement = document.getElementById('events');
+    eventsElement.addEventListener('mouseenter', changeEventText);
+    eventsElement.addEventListener('mouseleave', stopEventText);
+    
+
+
+    const competitionsElement = document.getElementById('competitions');
+    competitionsElement.addEventListener('mouseenter', changeCompetitionText);
+    competitionsElement.addEventListener('mouseleave', stopCompetitionText);
+    
+
+    const workshopElement = document.getElementById('workshops');
+    workshopElement.addEventListener('mouseenter', changeWorkshopText);
+    workshopElement.addEventListener('mouseleave', stopWorkshopText);
+   
+
+    const ambassdorElement = document.getElementById('ambassdor');
+    ambassdorElement.addEventListener('mouseenter', changeAmbassdorText);
+    ambassdorElement.addEventListener('mouseleave', stopAmbassdorText);
+    return () => {
+      ambassdorElement.removeEventListener('mouseenter', changeCompetitionText);
+      ambassdorElement.removeEventListener('mouseleave', stopCompetitionText);
+    };
+    return () => {
+      eventsElement.removeEventListener('mouseenter', changeEventText);
+      eventsElement.removeEventListener('mouseleave', stopEventText);
+    };
+    return () => {
+      workshopElement.removeEventListener('mouseenter', changeWorkshopText);
+      workshopElement.removeEventListener('mouseleave', stopWorkshopText);
+    };
+    return () => {
+      competitionsElement.removeEventListener('mouseenter', changeAmbassdorText);
+      competitionsElement.removeEventListener('mouseleave', stopAmbassdorText);
+    };
+  }, []);
+
+
+
+
+
+
+
+
 
 
   return (
@@ -17,10 +140,10 @@ function Header() {
               <Link to="/">  <img src={logo} alt="Logo"/> </Link>
             </div>
             <ul className="navbar-links">
-                <li><Link to="/events">{displayText}</Link></li>
-                <li><Link to="/competitions">Competitions</Link></li>
-                <li><Link to="/workshops">Workshops</Link></li>
-                <li><Link to="/pr_intern">PR Intern</Link></li>
+                <li id="events"><Link to="/events">{eventsText}</Link></li>
+                <li id="competitions"><Link to="/competitions">{competitionText}</Link></li>
+                <li id="workshops"><Link to="/workshops">{workshopsText}</Link></li>
+                <li id="ambassdor"><Link to="/pr_intern">{ambassadorText}</Link></li>
             </ul>
             <div className="navbar-auth">
                 <Link to="/login">Login/SignUp</Link>
